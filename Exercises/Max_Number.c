ecb076ca8af7f91c216025b036726583abd99acc
Alex <alexvasile470@gmail.com>
2026-04-08 16:57:41 +0300

    added new exercises

---
+++ Exercises/Max_Number.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int MaxNumber(int max_number, int numbers[])
+{
+    int Index;
+    int max = numbers[0];
+
+    for (Index = 0; Index < max_number; Index++)
+    {
+        if(numbers[Index] > max )
+        {
+            max = numbers[Index];
+        }
+    }
+    
+    return max;
+    
+}
+int main()
+{
+    int max_number = 32967;
+   
+    int numbers[max_number];
+    int Index;  
+    printf("INTRODUCETI CATE NUMERE DORITI SA VERIFICATI:\n");
+    scanf("%d", &max_number);
+    for (Index = 0; Index < max_number; Index++)
+    {
+        printf("INTRODUCETI NUMARUL %d:\n", Index + 1);
+        scanf("%d", &numbers[Index]);
+    }
+    
+printf("NUMARUL MAXIM ESTE: %d\n", MaxNumber(max_number, numbers));
+
+    return 0;
+}
